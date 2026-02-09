1d887007aede344119a560962660c927a40b1bb8
Sergey Nam <shaqk702@gmail.com>
2026-02-09 12:29:36 -0800

    Add CodeSignal Practice Problem - Recovery

---
+++ practice_assessments/recovery/main.py
@@ -0,0 +1,50 @@
+def solution(line):
+    atLeastOneDigit = False
+    if line[len(line) - 1] == '#':
+        i = 0
+        base = 0
+
+        # missing code start
+        hasBaseDigit = False
+        while i < len(line) and line[i] != '#':
+            if line[i] == '_':
+                i += 1
+                continue
+            if '0' <= line[i] <= '9':
+                base = base * 10 + (ord(line[i]) - ord('0'))
+                hasBaseDigit = True
+            else:
+                return False
+            i += 1
+
+        if i == len(line) or line[i] != '#':
+            return False
+        if not hasBaseDigit:
+            return False
+        # missing code start
+
+        if base < 2 or base > 16:
+            return False
+        i += 1
+        while i < len(line) - 1:
+            if line[i] != '_':
+                digit = -1
+                if 'a' <= line[i] and line[i] <= 'f':
+                    digit = ord(line[i]) - ord('a') + 10
+                if 'A' <= line[i] and line[i] <= 'F':
+                    digit = ord(line[i]) - ord('A') + 10
+                if '0' <= line[i] and line[i] <= '9':
+                    digit = ord(line[i]) - ord('0')
+                if 0 <= digit and digit < base:
+                    atLeastOneDigit = True
+                else:
+                    return False
+            i += 1
+    else:
+        for i in range(len(line)):
+            if line[i] != '_':
+                if '0' <= line[i] and line[i] <= '9':
+                    atLeastOneDigit = True
+                else:
+                    return False
+    return atLeastOneDigit
